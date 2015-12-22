509de2f1e381a671b1c8c0272aff7bfb6cff3b00
Peter Mumenthaler <pmumenthaler@gmail.com>
2015-12-22 22:48:54 +0100

    fix copy function, some enhancements and less code

--- sortmypics.py
+++ sortmypics.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
-# version 0.2 
+# version 0.3 
 # TODO: - document
-#       - fix copy function
 #       - header
 
 
@@ -26,6 +25,8 @@ class SortMyPics:
   "Constructor" 
   def __init__(self):
     self.__answer = ""
+    self.__pics = []
+    self.__sortyourself = []
     locale.setlocale(locale.LC_ALL,'de_CH.UTF-8')
 
   """Return parsed args. If destination directory is empty return None"""
@@ -40,6 +41,7 @@ class SortMyPics:
     else:
       parser.print_help()
       return None
+
   """Get date of pics and make destination directories""" 
   def make_destination_directories(self,src_dir, dest_dir):
     try:
@@ -50,62 +52,62 @@ class SortMyPics:
          tags = exifread.process_file(f, stop_tag='DateTimeOriginal')
          f.close()
          date = tags.get('EXIF DateTimeOriginal')
+         dir_string = ""
          if date:
            # 2013:08:10 14:54:03
            dat = datetime.strptime(str(date),'%Y:%m:%d %H:%M:%S')
-           dir_string = dat.strftime("/%Y/%B/")
-           try:
-             if dest_dir:
-               dir_string = dest_dir+dir_string
-               os.makedirs(dir_string)
-           except OSError as exc: # Python >2.5
-             if exc.errno == errno.EEXIST:
-               if not os.path.isdir(dir_string):
-                 answer = ask_if_overwrite(dir_string)
-                 if answer == "y" or answer == "a":
-                   try:
-                     os.makedirs(dir_string)
-                   except OSError as exc:
-                     if exc.errno == errno.EEXIST:
-                        pass
-                     else:
-                       raise
-             else:
-               raise
+           dir_string = dat.strftime("%Y/%B/")
+           self.__pics.append((src_dir+pic,dest_dir+dir_string))
          else:
-           print "file not a pic %s" %pic
+           print "file not a pic %s or no exif date" %pic
+           dir_string = "sortyourself/"
+           self.__sortyourself.append((src_dir+pic,dest_dir+dir_string))
+         try:
+           os.makedirs(dest_dir+dir_string)
+         except OSError as exc: # Python >2.5
+           if exc.errno == errno.EEXIST:
+             if not os.path.isdir(dir_string):
+               try:
+                 os.makedirs(dest_dir+dir_string)
+               except OSError as exc:
+                 if exc.errno == errno.EEXIST:
+                   pass
+                 else:
+                   raise
+           else:
+             raise
+
     except OSError as e:
      print e
      print "Directory does not contain any files"
  
-  def copy_pics(self,src_dir, dest_dir):
-    pics = os.listdir(src_dir)
-    dir_string = dest_dir
-    for pic in pics:
-      print "copying %s to %s"%(src_dir+pic,dir_string)
-      if os.path.exists(dir_string+pic):
-        anwser = self.ask_if_overwrite(pic)
-        if answer == "y" or answer == "a":
-          shutil.copy2(src_dir+pic,dir_string)
+  def copy_pics(self,pics):
+    for pic,dir_string in pics:
+      (head,tail) = os.path.split(pic)
+      dir_string = dir_string+tail
+      print "ldldld %s" %(dir_string)
+      if os.path.exists(dir_string):
+        self.ask_if_overwrite(pic)
+        if self.__answer == "y" or self.__answer == "a":
+          shutil.copy2(pic,dir_string)
           print "overwriting %s to %s"%(pic,dir_string)
-        if answer == "s" :
+        if self.__answer == "s" :
            print "skipping %s" %pic
-        else:
-           shutil.copy2(src_dir+pic,dir_string)
       else:
-         if not os.path.exists("./sortyourself"):
-           os.makedirs("./sortyourself")
-         shutil.copy2(src_dir+pic,"./sortyourself")
-         print "copying %s to %s"%(pic,"./sortyourself")
+           shutil.copy2(pic,dir_string)
 
   def ask_if_overwrite(self,path):
-    if answer == "a" or answer == "s":
-      return answer
-    while answer != "y" and answer != "n" and answer != "a" and answer != "s" :
-      answer = raw_input("%s file or directory exists. Do you want to overwrite?(Y/N/A (ALL) /S (Skipp all) )"%path)
-    return str.lower(answer)
+    if self.__answer == "a" or self.__answer == "s":
+      return 
+    while (self.__answer == "y" or self.__answer == "n") or (self.__answer != "a" and self.__answer != "s") :
+      self.__answer = raw_input("%s file or directory exists. Do you want to overwrite?(Y/N/A (ALL) /S (Skipp all) )"%path)
+    str.lower(self.__answer)
 
+  def get_pics(self):
+    return self.__pics
 
+  def get_sortyourself(self):
+    return self.__sortyourself
 
 
 if __name__ == "__main__":
@@ -116,7 +118,8 @@ if __name__ == "__main__":
    if options.prefix:
      prefix = options.prefix
    smp.make_destination_directories(options.directory,prefix)
-   smp.copy_pics(options.directory,prefix)
+   smp.copy_pics(smp.get_pics())
+   smp.copy_pics(smp.get_sortyourself())
 
    
 
